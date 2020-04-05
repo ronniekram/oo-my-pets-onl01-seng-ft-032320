d22eeb97f812d730aa54576d54515f70d1b2b1d2
Arynn Boniface <arynn.boniface@gmail.com>
2020-04-05 00:24:24 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,88 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_accessor :cat, :dog
+  attr_reader :name, :species
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = "human"
+    save
+  end
+  
+  def save
+    @@all << self
+  end 
+  
+  def self.all
+    @@all 
+  end 
+  
+  def self.count
+    @@all.count
+  end 
+  
+  def self.reset_all
+    @@all.clear
+  end 
+  
+  def say_species
+    "I am a human."
+  end 
+  
+  def cats
+    my_cats = []
+    Cat.all.each do |cat|
+      if cat.owner == self 
+        my_cats << cat 
+      end 
+    end 
+    my_cats
+  end 
+  
+  def dogs 
+    my_dogs = []
+    Dog.all.each do |dog|
+      if dog.owner == self 
+        my_dogs << dog 
+      end 
+    end 
+    my_dogs
+  end
+  
+  def buy_cat(name)
+    Cat.new(name, self)
+  end 
+  
+  def buy_dog(name)
+    Dog.new(name, self)
+  end
+  
+  def feed_cats
+    cats.map {|cat| cat.mood = "happy"}
+  end 
+  
+  def walk_dogs
+    dogs.map {|dog| dog.mood = "happy"}
+  end
+  
+  def sell_pets
+    cats.map do |cat|
+      cat.mood = "nervous"
+      cat.owner = nil
+    end
+    
+    dogs.map do |dog|
+      dog.mood = "nervous"
+      dog.owner = nil
+    end 
+  end
+  
+  def list_pets
+    dog_counter = 0 
+    cat_counter = 0
+    cats.each {|cat| cat_counter += 1}
+    dogs.each {|dog| dog_counter += 1}
+    "I have #{dog_counter} dog(s), and #{cat_counter} cat(s)."
+  end  
 end
